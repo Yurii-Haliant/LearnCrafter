8d27299e72abfbb48752edc266626d117f8dbdb6
Yurii-Haliant <133204650+Yurii-Haliant@users.noreply.github.com>
2025-11-14 23:36:49 +0100

    file

---
+++ node.js
@@ -0,0 +1,41 @@
+
+document.querySelector('.b1-1').addEventListener('click', () => {
+    document.querySelector('.hero').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+document.querySelector('.hero button').addEventListener('click', () => {
+    document.querySelector('.advantages').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+/* =========================================
+   3. Перемикач теми (Light/Dark Mode)
+========================================= */
+
+// Кнопка перемикання теми
+const themeBtn = document.getElementById('themeToggle');
+
+// Перевіряємо, чи є збережена тема у localStorage
+let currentTheme = localStorage.getItem('theme');
+
+// Якщо тема була темна — застосовуємо її
+if (currentTheme === 'dark') {
+    document.body.classList.add('dark');
+    themeBtn.textContent = '☀ Світла тема';
+}
+
+// Перемикаємо тему при кліку
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+
+    if (document.body.classList.contains('dark')) {
+        localStorage.setItem('theme', 'dark');
+        themeBtn.textContent = '☀ Світла тема';
+    } else {
+        localStorage.setItem('theme', 'light');
+        themeBtn.textContent = '🌙 Темна тема';
+    }
+});
